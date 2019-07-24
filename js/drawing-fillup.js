64bbc07871a1c63733e1beddbf189324e43e6bc6
FrancoHKTech <franco20131300@gmail.com>
2019-07-24 10:05:44 +0800

    Franco-24/7-10:05

--- js/drawing-fillup.js
+++ js/drawing-fillup.js
@@ -1,4 +1,4 @@
-class DrawingStraightLine extends PaintFunction{
+class BucketFill extends PaintFunction{
     constructor(contextReal,contextDraft){
         super();
         this.contextReal = contextReal;
@@ -9,10 +9,17 @@ class DrawingStraightLine extends PaintFunction{
     http://richmond.com/app/Widgetes/colorfill/nutzy-colorfill.html
     -   I want to fill up the area
     -   I need to identify the color of current area
-    -  contextReal.getImageData(0, 0, 1, 1); this is how you capture data
-    var imageData = contextReal.getImageData(0, 0, 100, 100);
+    -  var result = contextReal.getImageData(0, 0, 50, 50); this is how you capture data
+    - ImageData
+    - filling color: 
+      var imageData = contextReal.getImageData(0, 0, 100, 100);
+      contextReal.putImageData(imageData, 600, 600);
+    
 contextReal.getImageData(0, 0, 2, 2);
     imageData.data= [203,53,148,255]
+
+    contextReal.putImageData(imageData, 600, 600);
+
 
     var imageData = context.getImageData(0,0,canvasWidth,canvasHeight);
 
@@ -34,45 +41,70 @@ contextReal.getImageData(0, 0, 2, 2);
     
 
 
-
-
     */
-
-
-
-
 
     
     onMouseDown(coord,event){
         this.origX = coord[0];
         this.origY = coord[1];
+        var imgData = contextReal.getImageData(coord[0], coord[1], 1, 1);
+        console.log(this.origX);
+        console.log(this.origY);
+        console.log(imgData.data);
     }
-    onDragging(coord,event){
-        this.contextDraft.strokeStyle = "green";
-        this.contextDraft.lineWidth = 5;
-        this.contextDraft.beginPath();
-        this.contextDraft.moveTo(this.origX,this.origY);
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextDraft.lineTo(coord[0],coord[1]);
-        this.contextDraft.stroke();
-    }
+    // onDragging(coord,event){
+    //     this.contextDraft.strokeStyle = "green";
+    //     this.contextDraft.lineWidth = 5;
+    //     this.contextDraft.beginPath();
+    //     this.contextDraft.moveTo(this.origX,this.origY);
+    //     this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+    //     this.contextDraft.lineTo(coord[0],coord[1]);
+    //     this.contextDraft.stroke();
+    // }
 
-    onMouseMove(){}
     onMouseUp(coord){
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextReal.strokeStyle = "green";
-        this.contextReal.lineWidth = 5;
-        this.contextReal.beginPath();
-        this.contextReal.moveTo(this.origX,this.origY);
-        this.contextReal.lineTo(coord[0],coord[1]);
-        this.contextReal.stroke();
-        // History
-        history.push($('#canvas-real')[0].toDataURL());
-        //let currentHistoryIndex = history.length;
 
-        //console.log(currentHistoryIndex);
-        // console.log($('#canvas-real')[0].toDataURL());
-        // console.log(history);
+        /* 
+        1)  Now, I can capture the color of mousedown
+        2)  Go to the top of that column and put a mark-1
+        3)  check left and right and put a mark-2
+        4)  paint color on mark-1
+        
+
+
+
+
+        */
+        var imgData = contextReal.getImageData(coord[0], coord[1], 1, 1);
+        var r = imgData.data[0];
+        var g = imgData.data[1];
+        var b = imgData.data[2];
+        var a = imgData.data[3];
+
+        var nextImgData = contextReal.getImageData(coord[0], coord[1]+1, 1, 1);
+        var nextr = nextImgData.data[0];
+        var nextg = nextImgData.data[1];
+        var nextb = nextImgData.data[2];
+        var nexta = nextImgData.data[3];
+        
+        while(r == nextr && g == nextg && b == nextb && a == nexta){
+            var i = 1;
+            
+        };
+
+
+
+
+        // this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        // this.contextReal.strokeStyle = "green";
+        // this.contextReal.lineWidth = 5;
+        // this.contextReal.beginPath();
+        // this.contextReal.moveTo(this.origX,this.origY);
+        // this.contextReal.lineTo(coord[0],coord[1]);
+        // this.contextReal.stroke();
+        // // History
+        // history.push($('#canvas-real')[0].toDataURL());
+  
 
     }
     onMouseLeave(){}
