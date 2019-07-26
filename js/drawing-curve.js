7bfab088fb6861733358cffea4ac90f75b7cf713
Ivan <wonderboomhk@gmail.com>
2019-07-26 09:56:35 +0800

    curve function added

--- js/drawing-curve.js
+++ js/drawing-curve.js
@@ -6,40 +6,97 @@ class DrawingCurve extends PaintFunction{
         this.contextDraft = contextDraft;
         this.selectedStrokeColour = selectedStrokeColour;
 
-    }
-
-    onSlider(){
-      this.contextDraft.beginPath();
-      this.contextDraft.fillRect(this.selectedCpX, this.selectedCpY, 10, 10);
-      this.contextDraft.stroke();
+        this.originalX = 0;
+        this.originalY = 0;
+        this.newX = 0;
+        this.newY = 0;
+        this.mousedownCounter = 0;
     }
 
     onMouseDown(coord,event){
-        this.origX = coord[0];
-        this.origY = coord[1];
+        if(this.mousedownCounter === 0 ){
+            this.origX = coord[0];
+            this.origY = coord[1];
+            this.originalX = this.origX;
+            this.originalY = this.origY;
+            console.log(this.mousedownCounter);
+            console.log('Origin Location:' + this.originalX +','+ this.originalY);
+        }
     }
 
     onDragging(coord,event){
-        this.contextDraft.beginPath();
         this.contextDraft.strokeStyle = this.selectedStrokeColour;
         this.contextDraft.lineWidth = this.selectedLineWidth;
-        this.contextDraft.moveTo(this.origX,this.origY);
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextDraft.quadraticCurveTo(this.selectedCpX, this.selectedCpY, coord[0], coord[1]);
-        this.contextDraft.stroke();
+
+        if(this.mousedownCounter === 0 ){
+            this.contextDraft.beginPath();
+            this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+            this.contextDraft.moveTo(this.originalX,this.originalY);
+            this.contextDraft.lineTo(coord[0],coord[1]);
+            this.contextDraft.stroke();
+            this.newX = coord[0];
+            this.newY = coord[1];
+            console.log('New Location:' + this.newX +','+ this.newY);
+            console.log(this.mousedownCounter);
+
+            this.contextDraft.beginPath();
+            this.contextDraft.fillRect((coord[0]+this.originalX)/2, (coord[1]+this.originalX)/2, 10, 10);
+            this.contextDraft.stroke();
+            this.contextDraft.fillStyle = 'lightgrey';
+
+        }
+
+        if(this.mousedownCounter === 1 ){
+            this.contextDraft.beginPath();
+            this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+            this.contextDraft.moveTo(this.originalX,this.originalY);
+            this.contextDraft.quadraticCurveTo(coord[0], coord[1], this.newX, this.newY);
+            this.contextDraft.stroke();
+            console.log(this.mousedownCounter);
+
+            this.contextDraft.beginPath();
+            this.contextDraft.fillRect(coord[0], coord[1], 10, 10);
+            this.contextDraft.stroke();
+
+
+
+
+        }
     }
 
-    onMouseMove(){}
+    onMouseMove(){
+
+
+    }
 
     onMouseUp(coord){
-        this.contextDraft.clearRect(this.selectedCpX, this.selectedCpY, 10, 10);
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextReal.beginPath();
-        this.contextReal.strokeStyle = this.selectedStrokeColour;
-        this.contextReal.lineWidth = this.selectedLineWidth;
-        this.contextReal.moveTo(this.origX,this.origY);
-        this.contextReal.quadraticCurveTo(this.selectedCpX, this.selectedCpY, coord[0], coord[1]);
-        this.contextReal.stroke();
+
+        if(this.mousedownCounter === 0){
+            this.mousedownCounter = 1;
+            console.log(this.mousedownCounter);
+        } else {
+            this.contextReal.strokeStyle = this.selectedStrokeColour;
+            this.contextReal.lineWidth = this.selectedLineWidth;
+
+            this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+            this.contextReal.beginPath();
+
+            this.contextReal.moveTo(this.originalX,this.originalY);
+            this.contextReal.quadraticCurveTo(coord[0], coord[1], this.newX, this.newY);
+            this.contextReal.stroke();
+
+
+
+            this.mousedownCounter = 0;
+            console.log(this.mousedownCounter);
+
+
+
+
+        }
+
+
+
 
         // History
         history.push($('#canvas-real')[0].toDataURL());
